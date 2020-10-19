ccf971f8bf15a013a32f0b26147aecb6ab315fff
oroninne <52898234+oroninne@users.noreply.github.com>
2020-10-19 13:54:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ while True:
 
   elif counter == 1:
     
-      output = "It seems that you are feeling quite " + feelings_list[0] + ". However, do remember that "+ encouragement_list[0] + "! Hope you feel better :)"  
+      output = "It seems that you are feeling quite " + feelings_list[0] + ". However, do remember "+ encouragement_list[0] + "! Hope you feel better :)"  
 
   else:
 
